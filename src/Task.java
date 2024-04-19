f364ed113c061f74344c20a3af3ac835f9185228
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-19 14:28:03 +0300

    Change2

--- src/Task.java
+++ src/Task.java
@@ -30,7 +30,7 @@ public class Task {
                 Pattern detectRole = Pattern.compile(roles[i]);
                 Matcher detecter = detectRole.matcher(textLines[j]);
                 if (detecter.find()){
-                    result[numberOfLine]=j+1+") "+textLines[j]+"\n";
+                    result[numberOfLine]=j+1+") "+textLines[j].substring(roles[i].length())+"\n";
                     numberOfLine++;
                 }
             }
